4b305faea488a404fa9508a79ce20b50ba29aa89
wangjie <wjss@pku.edu.cn>
2016-10-09 13:56:20 +0800

    add parse weather data

--- app/src/main/java/cn/edu/pku/wangjie/miniweather/MainActivity.java
+++ app/src/main/java/cn/edu/pku/wangjie/miniweather/MainActivity.java
@@ -12,11 +12,16 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -50,6 +55,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
             if(NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE) {
                 Log.d("myWeather","网络OK");
                 queryWeatherCode(cityCode);
+
             }
             else {
                 Log.d("myWeather","网络挂了");
@@ -87,6 +93,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
                     }
                     String responseStr = response.toString();
                     Log.d("myWeather",responseStr);
+                    parseXML(responseStr);
                 } catch (MalformedURLException e) {
                     e.printStackTrace();
                 } catch (IOException e) {
@@ -95,6 +102,90 @@ public class MainActivity extends Activity implements View.OnClickListener {
             }
         }).start();
 
+    }
+
+    private void parseXML(String xmlDate) {
+        try {
+            int fengxiangCount = 0;
+            int fengliCount = 0;
+            int dateCount = 0;
+            int highCount = 0;
+            int lowCount = 0;
+            int typeCount = 0;
+            XmlPullParserFactory fac = XmlPullParserFactory.newInstance();
+            XmlPullParser xmlPullParser = fac.newPullParser();
+            xmlPullParser.setInput(new StringReader(xmlDate));
+            int eventType = xmlPullParser.getEventType();
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                switch (eventType) {
+                    case XmlPullParser.START_DOCUMENT:
+                        break;
+                    case XmlPullParser.START_TAG:
+                    if (xmlPullParser.getName().equals("city")) {
+                        eventType = xmlPullParser.next();
+                        Log.d("myapp2","city:  " + xmlPullParser.getText());
+                    }
+                    else if (xmlPullParser.getName().equals("updatetime")) {
+                        eventType = xmlPullParser.next();
+                        Log.d("myapp2","updatetime:  " + xmlPullParser.getText());
+                    }
+                    else if (xmlPullParser.getName().equals("shidu")) {
+                        eventType = xmlPullParser.next();
+                        Log.d("myapp2","shidu:  " + xmlPullParser.getText());
+                    }
+                    else if (xmlPullParser.getName().equals("wendu")) {
+                        eventType = xmlPullParser.next();
+                        Log.d("myapp2","wendu:  " + xmlPullParser.getText());
+                    }
+                    else if (xmlPullParser.getName().equals("pm25")) {
+                        eventType = xmlPullParser.next();
+                        Log.d("myapp2","pm2.5:  " + xmlPullParser.getText());
+                    }
+                    else if (xmlPullParser.getName().equals("quality")) {
+                        eventType = xmlPullParser.next();
+                        Log.d("myapp2","quality:  " + xmlPullParser.getText());
+                    }
+                    else if (xmlPullParser.getName().equals("fengxiang") && fengxiangCount == 0) {
+                        eventType = xmlPullParser.next();
+                        Log.d("myapp2","fengxiang:  " + xmlPullParser.getText());
+                        fengxiangCount++;
+                    }
+                    else if (xmlPullParser.getName().equals("fengli") && fengliCount == 0) {
+                        eventType = xmlPullParser.next();
+                        Log.d("myapp2","fengli:  " + xmlPullParser.getText());
+                        fengliCount++;
+                    }
+                    else if (xmlPullParser.getName().equals("date") && dateCount == 0) {
+                        eventType = xmlPullParser.next();
+                        Log.d("myapp2","date:  " + xmlPullParser.getText());
+                        dateCount++;
+                    }
+                    else if (xmlPullParser.getName().equals("high") && highCount == 0) {
+                        eventType = xmlPullParser.next();
+                        Log.d("myapp2","high:  " + xmlPullParser.getText());
+                        highCount++;
+                    }
+                    else if (xmlPullParser.getName().equals("low") && lowCount == 0) {
+                        eventType = xmlPullParser.next();
+                        Log.d("myapp2","low:  " + xmlPullParser.getText());
+                        lowCount++;
+                    }
+                    else if (xmlPullParser.getName().equals("type") && typeCount == 0) {
+                        eventType = xmlPullParser.next();
+                        Log.d("myapp2","type:  " + xmlPullParser.getText());
+                        typeCount++;
+                    }
+                        break;
+                    case XmlPullParser.END_TAG:
+                        break;
+                }
+                eventType = xmlPullParser.next();
+            }
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
 
